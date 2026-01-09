6e43128dc280311128767a927571794bdf4fab7c
Victor Flores <20253ds136@utez.edu.mx>
2026-01-09 09:31:27 -0600

    Creacion del programa HolaMundo

---
+++ HolaMundo01/src/Main.java
@@ -0,0 +1,12 @@
+/*
+Nombre:Flores Villegas Victor Alexander
+Grupo:E
+Fecha:08/01/2026
+Texto: “Hola mundo”
+*/
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hola mundo");
+    }
+}
