10c2a98f7c333253432540d4ee7c3f72b83083a5
Victor Flores <20253ds136@utez.edu.mx>
2026-01-22 08:59:11 -0600

    creacion del proyecto ControlIfElseVictorFlores

--- ActividadBuenasPracticas/src/Main.java
+++ ActividadBuenasPracticas/src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+public class Main{
+    public static int CONTADOR = 0;
+    public static int INTERVALO_CONTADOR = 1;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-public class Main {
+        int valorContador = pedirEntero(sc, "Ingresa el numero para hacer la secuencia: ");
+        int resultadoSecuencia = sumarHastaN(valorContador);
+        System.out.println("El resultado es: "+resultadoSecuencia);
+    }
+
+    /**
+     * Se muestran los mensajes y se registran los valores ingresados
+     * @param sc -> declarado en el main
+     * @param mensaje -> es un parametro
+     * @return -> double
+     */
+    public static int pedirEntero(Scanner sc, String mensaje){
+        System.out.println(mensaje);
+        return sc.nextInt() ;
+    }
+
+    /**
+     * Este metodo calcula la secuencia del numero ingresado
+     * @param valorContador -> Parametro que acepta un int com entrada
+     * @return -> resultado del calculo de la secuencia
+     */
+    public static int sumarHastaN(int valorContador){
+        for (INTERVALO_CONTADOR=1; INTERVALO_CONTADOR<=valorContador; INTERVALO_CONTADOR++){
+            CONTADOR += INTERVALO_CONTADOR;
+        }
+        return CONTADOR;
+    }
 }
