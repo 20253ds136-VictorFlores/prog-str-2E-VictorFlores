9e25846781106f2995a73029f309f6142f95b8d8
Victor Flores <20253ds136@utez.edu.mx>
2026-02-26 08:56:06 -0600

    Inlcusion de la demoJavaFx

--- demoJavaFx/src/main/java/module-info.java
+++ demoJavaFx/src/main/java/module-info.java
@@ -5,5 +5,7 @@ module com.example.demojavafx {
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.demojavafx to javafx.fxml;
+    opens com.example.demojavafx.controllers to javafx.fxml;
     exports com.example.demojavafx;
+    exports com.example.demojavafx.controllers;
 }
